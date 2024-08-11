6a97419e731970620f8baeb4074cfbb380a90349
rasheed <demandwork247@gmail.com>
2024-08-11 14:41:30 +0100

    feat: add logger

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import authRoute from "./routes/auth.route.js";
 import eventRoute from "./routes/event.route.js";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import requestLogger from "./middlewares/logger.middleware.js";
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(requestLogger)
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/events", eventRoute);
 
---
+++ src/middlewares/logger.middleware.js
@@ -0,0 +1,19 @@
+import logger from "../../config/logger.js";
+
+const requestLogger = async (req, res, next) => {
+	const start = Date.now();
+
+
+	await next();
+
+	res.on("finish", () => {
+		const duration = Date.now() - start;
+		logger.info(
+			`${req.method} ${req.url} ${res.statusCode} ${duration}ms`
+		);
+	});
+
+	
+};
+
+export default requestLogger;
